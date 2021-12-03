075f2a411712b7d0c61141d4e6460db5018a6cea
michaeljamieson <michaeljamieson10@gmail.com>
2021-12-02 22:56:45 -0500

    broken inlogin user to save point, added from tutorial https://medium.com/wesionary-team/how-to-setup-authentication-in-react-typescript-and-redux-6e2dd5616e30

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -4,5 +4,27 @@ export enum ActionTypes {
     fetchTodos,
     deleteTodo
 }
+// export enum AuthenticationTypes {
+//     //user reducer types
+//     SET_AUTHENTICATED,
+//     SET_UNAUTHENTICATED,
+//     SET_USER,
+//     LOADING_USER,
+//     //UI reducer types
+//     SET_ERRORS,
+//     LOADING_UI,
+//     CLEAR_ERRORS
+// }
+//In types.ts we will define constants for user authentication and UI loading purpose
+
+//user reducer types
+export const SET_AUTHENTICATED='SET_AUTHENTICATED';
+export const SET_UNAUTHENTICATED='SET_UNAUTHENTICATED';
+export const SET_USER='SET_USER';
+export const LOADING_USER='LOADING_USER';
+//UI reducer types
+export const SET_ERRORS='SET_ERRORS';
+export const LOADING_UI='LOADING_UI';
+export const CLEAR_ERRORS='CLEAR_ERRORS';
 
 export type Action = FetchTodosAction | DeleteTodoAction;
