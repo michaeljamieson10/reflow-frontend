c134b2352cdac044a66481b6617b1a4e92c77dd8
michaeljamieson <michaeljamieson10@gmail.com>
2021-12-02 22:01:07 -0500

    finished typescrip portion of course stephen grinder complete developers guide

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore, applyMiddleware} from "redux";
+import {Provider} from "react-redux";
+import thunk from "redux-thunk";
+import {App} from "./components/App";
+import {reducers} from "./reducers";
 
-interface AppProps {
-    color?: string;
-}
-// interface AppState {
-//     counter: number;
-// }
-
-class App extends React.Component<AppProps> {
-    state = { counter: 0};
-    // constructor(props: AppProps) {
-    //     super(props);
-    //     this.state = { counter: 0};
-    // }
-
-    onIncrement = (): void => {
-       this.setState({ counter: this.state.counter + 1});
-    } ;
-    onDecrement = (): void => {
-        this.setState({ counter: this.state.counter - 1});
-    } ;
-    render(){
-        return (
-            <div>
-            <button onClick={this.onIncrement}>Inc</button>
-            <button onClick={this.onDecrement}>Dec</button>
-                {this.state.counter}
-            </div>
-        )
-    }
-}
+const store = createStore(reducers, applyMiddleware(thunk))
 
 ReactDOM.render(
-    <App color={"red"}/>,
+    <Provider store={store}>
+    <App />
+    </Provider>,
     document.querySelector("#root")
     )
