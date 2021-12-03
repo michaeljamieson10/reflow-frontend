6357e9865391bd40e626c83c9a16b38460fda691
michaeljamieson <michaeljamieson10@gmail.com>
2021-12-02 23:11:58 -0500

    appear to get prior to functiont o work tog et ui up

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -14,27 +14,31 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 //redux stuff
 import { connect } from 'react-redux';
 import {loginUser} from "../actions/userActions";
+import {Todo} from "../actions";
 const useStyles = makeStyles(theme => ({
     specialText:{
         color: theme.palette.common.black
     }
-});
-function Login(props) {
-    const classes = useStyles();
+}))
 
-}
+
+
+export const Login: React.FC = (props:any) => {
+    const classes = useStyles();
     const [values, setValues] = useState({
         email: "",
-    password: ""
-});
-    const [errors, setErrors] = useState({});
+        password: ""
+    });
+    const [errors,setErrors] = useState({});
     const [loading, setLoading] = useState(false);
+
     useEffect(() => {
         if (props.UI.errors) {
             setErrors(props.UI.errors);
         }
         setLoading(props.UI.loading);
     }, [props.UI])
+
     const handleSubmit = (e: any) => {
         e.preventDefault();
         setLoading(true);
@@ -49,104 +53,131 @@ function Login(props) {
     const handleChange = (e: any) => {
         e.persist();
         setValues(values => ({
- ...values,
+            ...values,
             [e.target.name]: e.target.value
-    }));
+        }));
     };
+
     return (
-        <Box>
-            <Box className={classes.box}>
-                <Typography variant=”h4">
-                <Box fontWeight={600} letterSpacing={3}>
-                    SIGN IN
-                </Box>
-            </Typography>
-        </Box>
-    <Container
-        component="main"
- maxWidth="md">
-<CssBaseline />
-    <Grid
-    container
-    alignContent="center"
- alignItems="center"
- justify="center"
+        <>
+        </>
+    );
+};
 
- spacing={3}>
-     <Grid
-    item
-    md={3}>
-        <img src={""} alt=”My Logo” />
-</Grid>
-    <Grid item md={9}>
-        <Paper>
-            <Box>
-                <TextField
-                    variant="outlined"
-                    margin="none"
-                    value={values.email}
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    type="email"
-                    onChange={handleChange}
-                    helperText={errors.email}
-                    error={errors.email ? true : false}
-                    />
-                <TextField
-                    variant="outlined"
-                    margin="normal"
-                    value={values.password}
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    onChange={handleChange}
-                    helperText={errors.password}
-                    error={errors.password ? true : false}
-                    />
-                    {errors.message && (
-                        <Typography variant=”body2">
-                    {errors.message}
-                        </Typography>
-                        )}
-                <Grid container>
-                    <Grid item sm={6} md={6}>
-                        <FormControlLabel
-
-                            control={
-                                <Checkbox value="remember" color='primary' />
-                            }
-                            label="Remember me"
-                            />
-                    </Grid>
-                    <Grid item sm={6} md={6}>
-                        <Link to="#">
-                            Forgot password?
-                    </Link>
-                </Grid>
-    </Grid>
-    <Button type="submit" variant=”contained” color=”primary” disabled={loading}>
-        Login
-        {loading && (<CircularProgress className={classes.loader} size={30} color=”secondary” />)}
-            </Button>
-
-            </Box>
-            </Paper>
-            </Grid>
-            </Grid>
-            </Container >
-            </Box>
-            )
-        }
-        //this map the states to our props in this functional component
-        const mapStateToProps = (state: any) => ({
-        user: state.user,
-        UI: state.UI
-    });
-        //this map actions to our props in this functional component
-        const mapActionsToProps = {
-        loginUser
-    };
-        export default connect(mapStateToProps, mapActionsToProps)(Login)
+//         setLoading(props.UI.loading);
+//     }, [props.UI])
+//     const handleSubmit = (e: any) => {
+//         e.preventDefault();
+//         setLoading(true);
+// //your client side validation here
+// //after success validation
+//         const userData = {
+//             email: values.email,
+//             password: values.password,
+//         };
+//         props.loginUser(userData, props.history);
+//     }
+//     const handleChange = (e: any) => {
+//         e.persist();
+//         setValues(values => ({
+//  ...values,
+//             [e.target.name]: e.target.value
+//     }));
+//     };
+//     return (
+//         <Box>
+//             <Box className={classes.box}>
+//                 <Typography variant=”h4">
+//                 <Box fontWeight={600} letterSpacing={3}>
+//                     SIGN IN
+//                 </Box>
+//             </Typography>
+//         </Box>
+//     <Container
+//         component="main"
+//  maxWidth="md">
+// <CssBaseline />
+//     <Grid
+//     container
+//     alignContent="center"
+//  alignItems="center"
+//  justify="center"
+//
+//  spacing={3}>
+//      <Grid
+//     item
+//     md={3}>
+//         <img src={""} alt=”My Logo” />
+// </Grid>
+//     <Grid item md={9}>
+//         <Paper>
+//             <Box>
+//                 <TextField
+//                     variant="outlined"
+//                     margin="none"
+//                     value={values.email}
+//                     fullWidth
+//                     id="email"
+//                     label="Email Address"
+//                     name="email"
+//                     type="email"
+//                     onChange={handleChange}
+//                     helperText={errors.email}
+//                     error={errors.email ? true : false}
+//                     />
+//                 <TextField
+//                     variant="outlined"
+//                     margin="normal"
+//                     value={values.password}
+//                     fullWidth
+//                     name="password"
+//                     label="Password"
+//                     type="password"
+//                     onChange={handleChange}
+//                     helperText={errors.password}
+//                     error={errors.password ? true : false}
+//                     />
+//                     {errors.message && (
+//                         <Typography variant=”body2">
+//                     {errors.message}
+//                         </Typography>
+//                         )}
+//                 <Grid container>
+//                     <Grid item sm={6} md={6}>
+//                         <FormControlLabel
+//
+//                             control={
+//                                 <Checkbox value="remember" color='primary' />
+//                             }
+//                             label="Remember me"
+//                             />
+//                     </Grid>
+//                     <Grid item sm={6} md={6}>
+//                         <Link to="#">
+//                             Forgot password?
+//                     </Link>
+//                 </Grid>
+//     </Grid>
+//     <Button type="submit" variant=”contained” color=”primary” disabled={loading}>
+//         Login
+//         {loading && (<CircularProgress className={classes.loader} size={30} color=”secondary” />)}
+//             </Button>
+//
+//             </Box>
+//             </Paper>
+//             </Grid>
+//             </Grid>
+//             </Container >
+//             </Box>
+//             )
+//         }
+//         //this map the states to our props in this functional component
+//         const mapStateToProps = (state: any) => ({
+//         user: state.user,
+//         UI: state.UI
+//     });
+//         //this map actions to our props in this functional component
+//         const mapActionsToProps = {
+//         loginUser
+//     };
+        // export default connect(mapStateToProps, mapActionsToProps)(Login)
